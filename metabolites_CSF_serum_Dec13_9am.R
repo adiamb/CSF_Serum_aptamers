529f9785b4f1d5fd6f040f48a816838281979671
Aditya Ambati <ambati@stanford.edu>
2017-01-19 12:04:07 -0800

    Update metabolites_CSF_serum_Dec13_9am.R

--- metabolites_CSF_serum_Dec13_9am.R
+++ metabolites_CSF_serum_Dec13_9am.R
@@ -39,15 +39,78 @@ serum_t$EXTID = row.names(serum_t)
 
 #######add in EMIDS into the data frame to match 
 serum_IDmatched=merge.data.frame(serum_t, serumIDS, by.x = "EXTID", by.y = "ExtIdentifier")
+colnames(serum_IDmatched)=paste0(colnames(serum_IDmatched), "_SERUM")
 ##########get IDS from the CSF file
 require(plyr)
 require(dplyr)
 CSFIDS=select(CSF, SampleDescription, ExtIdentifier)
 csf_IDmatched = merge.data.frame(csf_t, CSFIDS, by.x = "EXTID", by.y = "ExtIdentifier")
+colnames(csf_IDmatched)=paste0(colnames(csf_IDmatched), "_CSF")
 #######################################################################################
-merg1 = merge.data.frame(csf_IDmatched, serum_IDmatched, by.x = "SampleDescription", by.y = "SampleId")
-merg1$EXTID.x = NULL
-merg1$EXTID.y = NULL
+merg1 = merge.data.frame(csf_IDmatched, serum_IDmatched, by.x = "SampleDescription_CSF", by.y = "SampleId_SERUM")
+merg1$EXTID_CSF = NULL
+merg1$EXTID_SERUM = NULL
+colnames(merg1)=gsub(" ", "_", colnames(merg1)) ##remove spaces from colnames
+###perform cross-correlations between csf and serum app####################################### 
+##CSF_APP/Serum_APP with all other proteins in the panel
+which(colnames(merg1) == "Apo_E4_CSF") ####get column index
+metnames = merg1[-c(1, 167)]
+amyloid = merg1$Apo_E4_CSF
+df = data.frame(label = paste(names(metnames)), Estimate=numeric(length(metnames)), P.value=numeric(length(metnames)))
+for (i in 1:length(metnames)){
+  test = cor.test(amyloid, metnames[[i]], method = "p")
+  df$Estimate[i]=test$estimate
+  df$P.value[i]=test$p.value
+}
+df=df[order(df$P.value),]
+df=df[order(df$P.value),]
+adjustedp1=p.adjust(df$P.value, method = "hochberg")
+df=cbind(df, adjustedp1)
+df
+sigam_csf=filter(df, df$adjustedp1 <= 0.05)
+write_csv(sigam, path = '~/Dropbox/CSF metabolites/Significant_correlation_amyloidbeta_Dec7.csv')
+###############CSF_APP with other serum proteins##################################################
+which(colnames(merg1) == "amyloid_precursor_protein_SERUM") ####get column index
+require(stringr)
+metnames = merg1[str_detect(colnames(merg1), "_SERUM")]
+metnames = metnames[-1]
+amyloid = merg1$Apo_E4_SERUM
+df = data.frame(label = paste(names(metnames)), Estimate=numeric(length(metnames)), P.value=numeric(length(metnames)))
+for (i in 1:length(metnames)){
+  test = cor.test(amyloid, metnames[[i]], method = "p")
+  df$Estimate[i]=test$estimate
+  df$P.value[i]=test$p.value
+}
+df=df[order(df$P.value),]
+adjustedp1=p.adjust(df$P.value, method = "hochberg")
+df=cbind(df, adjustedp1)
+df
+require(plyr);require(dplyr)
+sigam_csf=filter(df, df$adjustedp1 <= 0.05)
+write_csv(sigam, path = '~/Dropbox/CSF metabolites/Significant_correlation_amyloidbeta_Dec7.csv')
+
+#########################K-means clustering of all the 1725 proteins################################
+
+total_matrix = data.matrix(merg1[-1])
+total_matrix1_cor = cor(total_matrix)
+total1_dist = dist(total_matrix1_cor, method = "e")
+total1_cluster = hclust(total1_dist, method = "complete")
+plot(total1_cluster)
+
+clusters1=cutree(total1_cluster, h=11)
+clusters2 = as.data.frame(clusters1)
+clusters2$TargetID = row.names(clusters2)
+clusters2
+write.csv(clusters2, file = '~/Dropbox/total_serum_CSF_clusters_jan13.csv')
+d <- cut(as.dendrogram(total1_cluster), h=10)
+
+plot(d$lower[[1]])
+
+#####################################################################################################
+merg2= setNames(data.frame(t(merg1[,-1])), merg1[,1]) ##transpose the data frame
+colnames(merg1) = gsub("\\.x", "_CSF", colnames(merg1))
+colnames(merg1) = gsub("\\.y", "_SERUM", colnames(merg1))
+colnames(merg1)
 merg2= setNames(data.frame(t(merg1[,-1])), merg1[,1])
 merg2$TargetIDS = row.names(merg2)
 merg2$source=ifelse(grepl("\\.x", merg2$TargetIDS), "CSF", "SERUM")
@@ -62,6 +125,9 @@ merg2_list$SERUM$TargetIDS[!merg2_list$SERUM$TargetIDS %in% merg2_list$CSF$Targe
 
 #############################
 library(limma)
+require(reshape2)
+require(data.table)
+setDT(merg2)
 mod1=dcast(merg2, TargetIDS~source,fun.aggregate = length, value.var = values)
 mod1$TargetIDS = NULL
 vennCounts(mod1) %>% vennDiagram()
@@ -118,7 +184,10 @@ arrange(total_clusters, desc(CSF_ratio))
 write.csv(total_clusters, file ='~/Desktop/Total_Clusters_Dec5.csv')
 #######################subgroup###############################################analysis
 csf_groups = merge.data.frame(csf_IDmatched, revised_groups, by.x = "SampleDescription", by.y = "EMID") ##get diagnosis information by merge
+csf_groups$totalAPO = (csf_groups$`Apo E2`+csf_groups$`Apo E3`+csf_groups$`Apo E4`)/3
 serum_groups = merge.data.frame(serum_IDmatched, revised_groups, by.x = "SampleId", by.y = "EMID") 
+serum_groups$totalAPO = (serum_groups$`Apo E2`+serum_groups$`Apo E3`+serum_groups$`Apo E4`)/3
+
 ############ NARC -CSF ############################
 csf_groups[1:10]
 csf_narc = filter(csf_groups, Group == "1")
@@ -225,6 +294,9 @@ colnames(serum_narc_K_1)[1] = c("serum_NARCOLEPSY")
 colnames(serum_ctrl_K_1)[1] = c("serum_CTRLS")
 colnames(serum_total_allgrp_1)[1] = c("TOTAL_BOTH")
 
+n_ser=serum_narc_K_1[grep(7, serum_narc_K_1$serum_NARCOLEPSY),]
+c_ser=serum_ctrl_K_1[grep(7, serum_ctrl_K_1$serum_CTRLS),]
+
 
 ################################################ samr ###############################################
 require(samr) ##csf
@@ -312,20 +384,19 @@ csf_groups1$Group = NULL
 csf_groups1$SampleDescription = NULL
 
 which(colnames(csf_groups1) == "amyloid precursor protein") ####get column index
-metnames = csf_groups1[2:615]
-amyloid = csf_groups1$HCRT
-df = data.frame(label = paste(names(csf_groups1[2:615])), Estimate=numeric(614), P.value=numeric(614))
+metnames = csf_groups1[-c(616:621, 219)]
+amyloid = csf_groups1$`amyloid precursor protein`
+df = data.frame(label = paste(names(metnames)), Estimate=numeric(length(metnames)), P.value=numeric(length(metnames)))
 for (i in 1:length(metnames)){
   test = cor.test(amyloid, metnames[[i]], method = "p")
   df$Estimate[i]=test$estimate
   df$P.value[i]=test$p.value
 }
 df=df[order(df$P.value),]
-df=df[order(df$P.value),]
-adjustedp1=p.adjust(df$P.value, method = "hochberg")
+adjustedp1=p.adjust(df$P.value, method = "b")
 df=cbind(df, adjustedp1)
 df
-sigam=filter(df, df$adjustedp1 <= 0.05)
+sigam_csf=df#filter(df, df$adjustedp1 <= 0.05)
 write_csv(sigam, path = '~/Dropbox/CSF metabolites/Significant_correlation_amyloidbeta_Dec7.csv')
 
 sig_amyloid_all=csf_groups1[colnames(csf_groups1)%in% sigam$label]
@@ -392,8 +463,34 @@ heatmap.2(mat, trace="none",col=greenred(10), Rowv = NA, dendrogram = "col", Col
 ###only ctrl groups#####
 
 heatmap.2(csf_ctrl_matrix, trace="none",col=greenred(10), Rowv = NA, dendrogram = "col", Colv = as.dendrogram(csf_ctrl1_cluster),density.info = "none", key = F)
+##############################################################serum##############################################################################################
+serum_groups1 =serum_groups# filter(serum_groups, serum_groups$newgroups == 2 | serum_groups$newgroups == 3)
+serum_groups1$SampleDescription = NULL
+serum_groups1$EXTID = NULL
+serum_groups1$Group = NULL
+serum_groups1$SampleDescription = NULL
 
-
+which(colnames(serum_groups1) == "amyloid precursor protein") ####get column index
+metnames = serum_groups1[-c(1, 1111:1116, 349)]
+amyloid = serum_groups1$`amyloid precursor protein`
+df = data.frame(label = paste(names(metnames)), Estimate=numeric(1108), P.value=numeric(1108))
+for (i in 1:length(metnames)){
+  test = cor.test(amyloid, metnames[[i]], method = "p")
+  df$Estimate[i]=test$estimate
+  df$P.value[i]=test$p.value
+}
+df_serum=df[order(df$P.value),]
+adjustedp1_serum=p.adjust(df_serum$P.value, method = "hochberg")
+df=cbind(df_serum, adjustedp1_serum)
+df
+sigam_serum=filter(df, df$adjustedp1 <= 0.05)
+write_csv(sigam_serum, path = '~/Dropbox/serum metabolites/Significant_correlation_amyloidbeta_serum_Jan11.csv')
+###############################################compare correlations between csf serum ayloid and serum amyloid################################
+sigam_all=merge.data.frame(sigam_serum, sigam_csf, by.x = "label", by.y = "label", all = T)
+colnames(sigam_all) = gsub(".x", "_SERUM", colnames(sigam_all))
+colnames(sigam_all) = gsub(".y", "_CSF", colnames(sigam_all))
+sigam_all
+arrange(sigam_all, adjustedp1_CSF, adjustedp1_SERUM)
 ############################limma ####################
 require(Biobase)
 require(limma)
@@ -405,6 +502,164 @@ object
 design1 = model.matrix(~y)
 fit = eBayes(lmFit(object, design1))
 topTable(fit, coef = "y", adjust = "BH", p = 40)
+
+require(plyr)
+require(dplyr)
+###retreive clusters for ctrl csf and narc csf 
+csf_narc_K_1[grep("amy", csf_narc_K_1$TargetIDs),]
+csf_narc_K_1[grep(3, csf_narc_K_1$CSF_NARCOLEPSY),] %>% write.csv(file = '~/Desktop/NARC_cluster.csv')
+
+csf_ctrl_K_1[grep("amy", csf_ctrl_K_1$TargetIDs),]
+csf_ctrl_K_1[grep(3, csf_ctrl_K_1$CSF_CTRLS),] %>% write.csv(file = '~/Desktop/CTRL_cluster.csv')
+
+
+##get all serum and csf protein names from the tow files
+serumnames=select(serum1, TargetFullName, Target)
+csfnames = select(csf1, TargetFullName, Target)
+
+##generate boxplots for checking normalilty 
+serum_melt=melt(serum_groups1, id.vars = c("SampleId","DbID","Dx","0602","HCRT","oldgroups","newgroups"))
+ggplot(serum_melt, aes(factor(variable), value))+geom_boxplot()+theme(axis.text.x = element_text(angle = 90))
+csf_melt=melt(csf_groups, id.vars = c("SampleDescription", "EXTID","DbID","Dx","0602","HCRT","oldgroups","newgroups"))
+ggplot(csf_melt, aes(factor(variable), value))+geom_boxplot()+theme(axis.text.x = element_text(angle = 90))
+
+###back convert log2 to raw values for CSF_group2
+csf_rawEM = cbind.data.frame(csf_groups1[c(616:621)], csf_groups1[-c(616:621)])
+csfraw_melt=melt(csf_rawEM, id.vars = c(names(csf_groups1[c(616:621)])))
+csf_dilEM=select(csf1, Dilution, Target)  
+csfraw_melt = as_data_frame(csfraw_melt)
+csfraw_dilEM_melt=merge.data.frame(csfraw_melt, csf_dilEM, by.x = "variable", by.y = "Target", all.x = T)
+csfraw_dilEM_melt$Dilution[is.na(csfraw_dilEM_melt$Dilution)] = 15
+setDT(csfraw_dilEM_melt)
+csf_agg=csfraw_dilEM_melt[, list(value = median(value)), by =list(variable, Dilution)]
+##read in the japanese soma data
+require(readr)
+jap=read_delim('SOMA_data.txt', delim = "\t", skip = 6)
+
+###get dilutions for each csf protein from the japanese cohort
+jap_dil=read_delim('SOMA_data.txt', delim = "\t", skip = 3)
+dil_CSFjap =jap_dil[1:3,]
+dil_CSFjap2=data.frame(t(dil_CSFjap[-c(1:3)]))
+colnames(dil_CSFjap2)=c("Dilution", "Units", "Target")
+dil_CSFjap2=dil_CSFjap2[-1,]
+dil_CSFjap2$Dilution=gsub("%", "", dil_CSFjap2$Dilution) %>% as.numeric() %>% round(digits =3)  
+setDT(jap)
+jap_melt=melt(jap, id.vars = c("Sample", "1=men; 2=women", "years", "Target")) 
+jap_dil_melt = merge.data.frame(jap_melt, dil_CSFjap2, by.x = "variable", by.y = "Target", all.x = T)
+jap_dil_melt$Dilution[is.na(jap_dil_melt$Dilution)] = 0.05
+setDT(jap_dil_melt)
+jap_csf_agg=jap_dil_melt[, list(value = median(value)), by = list(variable, Dilution)]
+############################################################################################ merge the aggregated data between japanese and our cohort and make plots######################################33
+csf_agg_jap_ours=merge.data.frame(jap_csf_agg, csf_agg, by.x = "variable", by.y = "variable", suffixes = c("_JAP", "_OURS"))
+##merge in the kmeans clusters 
+csf_agg_jap_oursk=merge.data.frame(csf_agg_jap_ours, csf_total_K, by.x = "variable", by.y = "TargetIDs")
+require(ggplot2)
+ggplot(csf_agg_jap_oursk, aes(log2(value_JAP), value_OURS, size = factor(Dilution_JAP), label = variable))+geom_point(alpha = 0.3)+geom_text_repel(size = 2.5)+facet_wrap(~TOTAL_BOTH, scales = "free")
+ggplot(csf_agg_jap_oursk, aes(log2(value_JAP), value_OURS, size = factor(Dilution_JAP), label = variable, color = factor(TOTAL_BOTH)))+geom_point(alpha = 0.3)+facet_wrap(~TOTAL_BOTH, scales = "free")
+
+ggplot(csf_agg_jap_ours, aes(value_JAP, value_OURS, size = Dilution_JAP, label = variable))+geom_text_repel()+facet_wrap(~TOTAL_BOTH, scales = "free")
+
+############################################################################################
+jap$totalAPO = (jap$`Apo E2`+jap$`Apo E3`+jap$`Apo E4`)/3
+metnames = log2(jap[-c(1:4, 330)])
+amyloid =log2(jap$`amyloid precursor protein`)
+df = data.frame(label = paste(names(metnames)), Estimate=numeric(length(metnames)), P.value=numeric(length(metnames)))
+for (i in 1:length(metnames)){
+  test = cor.test(amyloid, metnames[[i]], method = "p")
+  df$Estimate[i]=test$estimate
+  df$P.value[i]=test$p.value
+}
+df_jap=df[order(df$P.value),]
+adjustedp1_jap=p.adjust(df_jap$P.value, method = "hochberg")
+df=cbind(df_jap, adjustedp1_jap)
+df
+require(plyr);require(dplyr)
+sigam_jap=df#filter(df, df$adjustedp1_jap <= 0.05)
+sigam_jap
+
+#############correlation matrix of japanese data###############################################
+jap_cor = as.matrix(cor(jap[-c(1:4)]))
+jap_dist = dist(jap_cor, method = "e")
+jap_clust = hclust(jap_dist, method = 'complete')
+plot(jap_clust)
+d_jap <- cut(as.dendrogram(jap_clust), h=10)
+plot(d_jap$lower[[9]])
+
+require(HSAUR3)
+km    <- kmeans(scale(jap[-c(1:4)]), centers = 5)
+dissE <- daisy(jap[-c(1:4)]) 
+dE2   <- dissE^2
+sk2   <- silhouette(km$cl, dE2)
+plot(sk2)
+
+############################################SAMR analysis japanese data#####################################
+require(samr)
+#y = ifelse(jap$`1=men; 2=women` == "1", 1, 2)
+y=jap$years
+x = t(scale(jap[-c(1:4)]))
+d = list(x=x, y=y, geneid=rownames(x), logged2 =T)
+
+samr.obj<-samr(d,  resp.type="Quantitative", assay.type = "array", center.arrays = T, nperms = 1000)
+delta.table <- samr.compute.delta.table(samr.obj)
+delta.table
+delta=0.8
+samr.plot(samr.obj,delta)
+
+### create significant genes table
+siggenes.table<-samr.compute.siggenes.table(samr.obj,delta, d, delta.table)
+siggenes.table
+################################################Limma analysis ###############################################
+
+
+object = new("ExpressionSet", exprs = as.matrix(t(log2(jap[-c(1:4)]))))
+object
+design1 = model.matrix(~`1=men; 2=women`+years, data = jap)
+fit = eBayes(lmFit(object, design1))
+topTable(fit, coef = "`1=men; 2=women`", adjust = "BH", number = 50)
+topTable(fit, coef = "years", adjust = "BH", number = 50)
+volcanoplot(fit, coef = "`1=men; 2=women`",highlight=20, pch=20)
+
+
+
+
+
+
+############################################# merge the corre estimates between japan and ours ###
+csf_merge_JO = merge.data.frame(sigam_jap, sigam_csf, by.x = "label", by.y = "label", suffixes = c("_JAP", "_OURS"))
+################add in the dilution factors for the japanese
+dil_jP=select(csf_agg_jap_oursk, Dilution_JAP, variable)
+########################merge the dilutions for each protein########################################
+csf_merge_JO2 = merge.data.frame(csf_merge_JO, dil_jP, by.x = "label", by.y = "variable")
+
+ggplot(csf_merge_JO, aes(Estimate_JAP, Estimate_OURS, label = label))+geom_text(size = 3.5, angle = 90, check_overlap = T)#+geom_point(size = 5)+geom_text(label = csf_merge_JO$label,check_overlap = T)
+ggplot(csf_merge_JO2, aes(Estimate_JAP, Estimate_OURS, label = label, size = factor(Dilution_JAP)))+geom_point(alpha = 0.5)+geom_text(size = 3.5, angle = 45, check_overlap = T)
+################################################################################################################################
+#write out the final files for fresh analsysis 
+###from IDStoling get all the clinical data
+clinical_data=select(ids_ling, DbID, `ID to Sharhad (ST)`, Gender, Age, Race, BMI, `Interval from sleepiness(year)`, `Interval from cataplexy(year)`)
+clinical_data = merge.data.frame(revised_groups[-c(1, 3:5)], clinical_data, by.x = "EMID", by.y = "ID to Sharhad (ST)")
+###make final files #### for further analysis
+csf_final = merge.data.frame(clinical_data, csf_groups[-c(2, 618:623)], by.x = "EMID", by.y = "SampleDescription")
+write_csv(csf_final, path = '~/Dropbox/CSF metabolites/CSFProteinAptamers/CSF_FINAL_Jan18.csv')
+serum_final = merge.data.frame(clinical_data, serum_groups[-c(2, 1112:1117)], by.x = "EMID", by.y = "SampleId")
+write_csv(serum_final, path = '~/Dropbox/CSF metabolites/CSFProteinAptamers/SERUM_FINAL_Jan18.csv')
+write_csv(jap, path = '~/Dropbox/CSF metabolites/CSFProteinAptamers/JAPCSF_FINAL_Jan18.csv')
+##########merge final 
+Combined_final = merge.data.frame(clinical_data, merg1, by.x = "EMID", by.y = "SampleDescription_CSF")
+require(readr)
+write_csv(Combined_final, path = '~/Dropbox/CSF metabolites/CSFProteinAptamers/combined_FINAL_Jan18.csv')
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
